dd6876f717a158b6e2e45b60979cfbbd744e78ec
Benjamin Santos <Benjamin.Santos@ibm.com>
2025-04-28 11:40:45 -0400

    remove logging specific code

--- src/extension.ts
+++ src/extension.ts
@@ -1,17 +1,6 @@
 import * as vscode from 'vscode';
-import * as log4js from 'log4js';
-import { ZoweVsCodeExtension } from '@zowe/zowe-explorer-api';
 
 export function activate(context: vscode.ExtensionContext) {
-
-	const logger = log4js.getLogger();
-	logger.level = "debug"; // default level is OFF - which means no logs at all.
-	logger.debug("This will end up in the debug console");
-	ZoweVsCodeExtension.updateCredentials({}, ZoweVsCodeExtension.getZoweExplorerApi(
-		'2.0.0-SNAPSHOT'
-	));
-	logger.debug("This will end up in the imperative log");
-
 }
 
 // This method is called when your extension is deactivated
