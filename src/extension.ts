6a224196db12c0359575a51f952f288212b4de1b
Benjamin Santos <Benjamin.Santos@ibm.com>
2025-04-28 11:51:42 -0400

    Update extension.ts

--- src/extension.ts
+++ src/extension.ts
@@ -1,7 +1,14 @@
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
+import { ZoweVsCodeExtension } from "@zowe/zowe-explorer-api";
 
 export function activate(context: vscode.ExtensionContext) {
+  const zeApi = ZoweVsCodeExtension.getZoweExplorerApi("2.0.0-SNAPSHOT");
+  const defaultProfile = zeApi
+    .getExplorerExtenderApi()
+    .getProfilesCache()
+    .getDefaultProfile();
+  ZoweVsCodeExtension.updateCredentials({ profile: defaultProfile }, zeApi);
 }
 
 // This method is called when your extension is deactivated
-export function deactivate() { }
+export function deactivate() {}
